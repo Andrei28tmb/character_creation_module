aea5dde66b5833d17f22ec02e928fc11237df92a
Стас Басов <stas.basov@whatever.com>
2023-02-05 17:19:00 +0300

    Add new file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from random import randint
 from graphic_arts.start_game_banner import run_screensaver
 
+
 def attack(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный'
@@ -95,5 +96,3 @@ if __name__ == '__main__':
     char_class = choice_char_class()
     print(start_training(char_name, char_class))
 
-
-main()
